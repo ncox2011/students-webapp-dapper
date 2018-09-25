668f1443a2574b3e9497d7d4b236fa641f0a66ae
Natasha Cox <natashac1014@gmail.com>
2018-09-25 14:49:37 -0500

    added controllers

--- src/Models/Instructor.cs
+++ src/Models/Instructor.cs
@@ -1,15 +1,39 @@
-using Dapper;
-using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Workforce.Models
 {
     public class Instructor
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name="Slack Handle")]
+        [MaxLength(20)]
         public string SlackHandle { get; set; }
+
+        [Required]
+        [Display(Name ="Specialty")]
         public string Specialty { get; set; }
+
         public Cohort Cohort { get; set; }
+
+
+        [Display(Name = "Instructor Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
     }
 }
